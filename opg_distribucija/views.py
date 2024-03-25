d001dc8f407c5e303af4c8f876a3a706dcfd50ce
Josip Relic <rela1409@gmail.com>
2024-03-25 15:21:59 +0100

    Kreiranje pocetne stranice i prikupljanje static datoteka

--- opg_distribucija/views.py
+++ opg_distribucija/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('POČETNA STRANICA!')
+def pocetna_stranica(request):
+    return render(request, 'pocetna_stranica.html')
