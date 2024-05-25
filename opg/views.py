27f583ce142f7a7f52e436cd04854c3b6ee611d2
Josip Relic <rela1409@gmail.com>
2024-05-25 15:06:27 +0200

    Backend za ažuriranje podataka na opg profilu

--- opg/views.py
+++ opg/views.py
@@ -1,16 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from .forms import FormaOpg
+from .models import Opg
 from korisnicki_racuni.forms import KorisnickiProfilForma
 from korisnicki_racuni.models import KorisnickiProfil
-from .models import Opg
+
+
 # Create your views here.
 
 def opg_profil(request):
     profil = get_object_or_404(KorisnickiProfil, korisnik = request.user)
     opg = get_object_or_404(Opg, korisnik = request.user)
 
-    profil_forma = KorisnickiProfilForma(instance=profil)
-    opg_forma = FormaOpg(instance=opg)
+    if request.method == 'POST':
+        profil_forma = KorisnickiProfilForma(request.POST, request.FILES, instance=profil)
+        opg_forma = FormaOpg(request.POST, request.FILES, instance=opg)
+        if profil_forma.is_valid() and opg_forma.is_valid():
+            profil_forma.save()
+            opg_forma.save()
+            messages.success(request, 'Podatci su ažurirani.')
+            return redirect('opg_profil')
+        else:
+            print(profil_forma.errors)
+            print(opg_forma.errors)
+
+    else:
+        profil_forma = KorisnickiProfilForma(instance=profil)
+        opg_forma = FormaOpg(instance=opg)
 
     context = {
         'profil_forma': profil_forma,
