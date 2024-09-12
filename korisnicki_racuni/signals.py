ff406a84f2316b033651770467eada7f590a4c3d
Josip Relic <rela1409@gmail.com>
2024-09-12 23:35:02 +0200

    backup baze

--- korisnicki_racuni/signals.py
+++ korisnicki_racuni/signals.py
@@ -3,19 +3,19 @@ from django.dispatch import receiver
 from .models import User, KorisnickiProfil
 
 
-@receiver(post_save, sender=User)
-def kreiraj_profil_post_save_receiver(sender, instance, created, **kwargs):
-    print(created)
-    if created:
-        KorisnickiProfil.objects.create(korisnik=instance)
-    else:
-        try:
-            profil = KorisnickiProfil.objects.get(korisnik=instance) 
-            profil.save()
-        except:
-            # kreiraj korisnicki profil ukoliko ne postoji
-            KorisnickiProfil.objects.create(korisnik=instance)
-        print('korisnik je azuriran')
+#@receiver(post_save, sender=User)
+#def kreiraj_profil_post_save_receiver(sender, instance, created, **kwargs):
+#    print(created)
+#    if created:
+#        KorisnickiProfil.objects.create(korisnik=instance)
+#    else:
+#        try:
+#            profil = KorisnickiProfil.objects.get(korisnik=instance) 
+#            profil.save()
+#        except:
+#            # kreiraj korisnicki profil ukoliko ne postoji
+#            KorisnickiProfil.objects.create(korisnik=instance)
+#        print('korisnik je azuriran')
 
 
 @receiver(pre_save, sender=User)
